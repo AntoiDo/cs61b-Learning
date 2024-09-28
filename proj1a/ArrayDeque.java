c74d5907b1bbb39d1d4efab195104a27ff9b2a52
AntoiDo <cjhddb@foxmail.com>
2024-09-28 15:18:43 +0800

    change arrayDeque into circular array

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,81 +1,101 @@
-//todo:将arrayDeque改造成循环数组(using %)
 public class ArrayDeque<T> {
     private T[] array;
     private int size;
     private int front;
+    private int rear;
+
     public ArrayDeque() {
         array = (T[]) new Object[8];
         size = 0;
         front = 0;
+        rear = 0;
     }
+
     public int size() {
         return size;
     }
+
     public boolean isEmpty() {
         return size == 0;
     }
+
     public void printDeque() {
-        for(int i = front; i < size; i++){
-            System.out.print(array[i]);
-            if(i != size - 1){
+        int idx = front;
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[idx]);
+            if (i != size - 1) {
                 System.out.print(" ");
-            }else{
+            } else {
                 System.out.println();
             }
+            idx = (idx + 1) % array.length;  // Move to the next index circularly
         }
     }
+
     private void resize(int newLen) {
-        T[] newArray = (T[])new Object[newLen];
-        System.arraycopy(array, 0, newArray, 0, array.length);
+        T[] newArray = (T[]) new Object[newLen];
+        int idx = front;
+        for (int i = 0; i < size; i++) {
+            newArray[i] = array[idx];
+            idx = (idx + 1) % array.length;
+        }
+        front = 0;
+        rear = size;
         array = newArray;
     }
+
     public void addFirst(T item) {
-        if(size == array.length){
-            resize(size * 2);
+        if (size == array.length) {
+            resize(array.length * 2);
         }
-        for(int i = size; i > 0; i--){
-            array[i] = array[i-1];
-        }
-        array[0] = item;
-        size ++;
-    }
-    public void addLast(T item){
-        if(size == array.length){
-            resize(size * 2);
-        }
-        array[size++] = item;
-    }
-    public T removeFirst(){
-        if(size == 0){
-            return null;
-        }
-        T temp = array[0];
-        for(int i = 1; i < size; i++){
-            array[i-1] = array[i];
-        }
-        size --;
-        return temp;
-    }
-    public T removeLast(){
-        if(size == 0){
-            return null;
-        }
-        T temp = array[size - 1];
-        size -- ;
-        return temp;
-    }
-    public T get(int index){
-        return array[index];
+        front = (front - 1 + array.length) % array.length;  // Circularly move front
+        array[front] = item;
+        size++;
     }
 
+    public void addLast(T item) {
+        if (size == array.length) {
+            resize(array.length * 2);
+        }
+        array[rear] = item;
+        rear = (rear + 1) % array.length;  // Circularly move rear
+        size++;
+    }
 
-//    public static void main(String[] args){
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        T temp = array[front];
+        front = (front + 1) % array.length;  // Circularly move front
+        size--;
+        return temp;
+    }
+
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        rear = (rear - 1 + array.length) % array.length;  // Circularly move rear
+        T temp = array[rear];
+        size--;
+        return temp;
+    }
+
+    public T get(int index) {
+        if (index >= size || index < 0) {
+            return null;
+        }
+        int idx = (front + index) % array.length;
+        return array[idx];
+    }
+
+//    public static void main(String[] args) {
 //        ArrayDeque<Integer> deque = new ArrayDeque<>();
 //        deque.addFirst(1);
 //        deque.addFirst(2);
-//        deque.addFirst(3);
-//        deque.addLast(6);
-//        deque.removeLast();
+//        deque.addLast(3);
+//        deque.addFirst(4);
 //        deque.printDeque();
 //    }
 }
